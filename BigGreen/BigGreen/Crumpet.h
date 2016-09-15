9ac49bc1ed12f6b39b158010fe147fefb995624e
MartinXPN <mirakyanmartin@gmail.com>
2016-09-15 23:27:03 +0400

    comments added

--- BigGreen/BigGreen/Crumpet.h
+++ BigGreen/BigGreen/Crumpet.h
@@ -4,19 +4,26 @@
 #ifndef CRUMPET
 #define CRUMPET
 
+/*
+Crumpet is a subclass of Widget
+it can be instantiated
+*/
 class Crumpet : public Widget
 {
 private:
 	double markUpValue = 26.99;
 
 protected:
-	double manufacturingCost;
+	double manufacturingCost;	/// has to be provided by a user
 
 public:
+	/// Crumpet can only be constructed by providing both name and manufacturingCost
 	Crumpet(std::string name, double manufacturingCost);
 	~Crumpet();
 
+	/// override calculatePrice inherited from Widget
 	virtual double calculatePrice();
+	/// define new method of calculating sale price + 20% - 10%
 	double calculateSalePrice();
 };
 
