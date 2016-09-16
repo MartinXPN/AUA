c8e82b079b1665ab826426698b236f2c688c51ec
Student Generic Account <ADStudent@auaad.aua.am>
2016-09-16 13:25:56 +0400

    DataMining - started linear regression

--- DataMining/Class5/script.R
+++ DataMining/Class5/script.R
@@ -11,4 +11,35 @@ data <- read.csv( 'Titanic_imputed.csv' )
 View( data )
 
 data$pclass <- factor( data$pclass, labels = c( "rich", 'normal', 'poor' ), levels = c( 1, 2, 3 ) )
+data$survived <- factor( data$survived, labels = c( 'yes', 'no' ), levels = c( 1, 0) )
 View( data )
+
+
+Table <- table( data$survived, data$pclass )  # create table with 2 values (survived, pclass)
+Table                                         # show the table
+chisq.test( Table )                           # k square test
+
+TableGender <- table( data$survived, data$sex ) # create table with 2 values (survived, pclass)
+TableGender                                     # show the table
+chisq.test( TableGender )                       # k square test
+
+
+
+
+
+# BLOOD ANALYSIS
+blood <- read.table( 'blood pressure.txt', header = TRUE )
+View( blood )
+blood$Index <- NULL
+blood$One <- NULL
+
+
+# plot the relationship between age and pressure
+library(ggplot2)
+qplot( blood$Age, blood$Pressure )
+
+model <- lm( Pressure~Age, data = blood )
+summary( model )
+# => Pressure = 0.97*Age + 98.7
+
+
