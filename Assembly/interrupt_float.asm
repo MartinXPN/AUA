99160d0fcbb32ed03a9e26ef348aeeea768cc214
Martin-Mirakyan <mirakyanmartin@gmail.com>
2017-04-03 17:34:56 +0400

    Parse float up to .

--- Assembly/interrupt_float.asm
+++ Assembly/interrupt_float.asm
@@ -1,11 +1,30 @@
+##################################################
+#####                                       ######
+#####       Read Float with interrupt       ######
+#####       © 2017 All Rights Reserved.     ######
+#####                                       ######
+##################################################
+#####                                       ######
+#####    $s1 = current size of the buffer   ######
+#####    $s2 = &buffer[0]                   ######
+#####    $s3 = &buffer[size-1]              ######
+#####    $f1 = resulting float              ######
+#####    $f7 = 0.0                          ######
+#####                                       ######
+##################################################
+
+
 	.kdata					# kernel data
 s1:		.word 10
 s2:		.word 11
 prompt: 	.asciiz "Input float: "		# Prompt for user to input a floating point number
 bad_float:	.asciiz "\nInvalid float!\n"
+result_float:	.asciiz "\nResult float: "
 new_line: 	.asciiz "\n"
 float_buffer:	.space 100			# Reserve 100 bytes for float input
-
+zero:		.float 0.0
+point_one:	.float 0.1
+ten:		.float 10.0
 
 	.text
 	.globl main
@@ -23,6 +42,7 @@ main:				# Enable interrupts in main and jump to infinite loop of here:
 
 	la $s2, float_buffer	# Pointer to the first element of the buffer
 	la $s3, float_buffer	# $s3 is the current element where the modifications need to be done
+	lwc1 $f7, zero		# Store 0 in register $f7 as there is no default register with value 0 for floats like for integers...
 
 	li $v0,4		# print a string
 	la $a0, prompt 		# Prompt user to enter a float
@@ -108,8 +128,8 @@ check_float:
 	li $t2, 0		# Number of dots found so far
 
 	lp:
-		addi 	$t0, 1		# Move to the next cell
-		addi 	$t4, 1		# Add 1 to index ($t4)
+		addi 	$t0, 1			# Move to the next cell
+		addi 	$t4, 1			# Add 1 to index ($t4)
 		beq 	$s1, $t4, parse_float	# Break if $t4 reached the size of the buffer
 		lb    	$a0, 0($t0)     	# load byte into $a0
 		beq	$a0, $t3, found_dot	# continue the loop if current digit is '.'
@@ -128,8 +148,49 @@ check_float:
 		syscall
 		j exit
 
+# Parse string in the buffer to float in $f1
 parse_float:
-	j exit
+	lwc1 $f2, point_one	# $f2 = 0.1
+	lwc1 $f3, ten		# $f3 = 10.0
+	lwc1 $f4, point_one	# $f4 = 0.1 (exponent which will decrease by 0.1 in each step)
+	lwc1 $f1, zero		# $f1 = res = 0.0
+	li $t3, 46		# $t3 = '.'
+
+	li $t4, 0		# Counter for the loop
+	move $t0, $s2		# keep pointer to the first element of the buffer
+
+	parse_decimal_loop:
+		beq 	$s1, $t4, print_result		# Break if $t4 reached the size of the buffer
+		lb    	$t1, 0($t0)     		# load byte into $t1
+		beq	$t1, $t3, parse_floating_loop	# parse digits after floating point
+		addi	$t1, -48			# Get value of the character
+		
+		mtc1 	$t1, $f12		# move to c1 value stored in t1
+ 		cvt.s.w $f6, $f12		# Convert to float
+
+		mul.s 	$f1, $f1, $f3		# res *= 10
+		add.s 	$f1, $f1, $f6 		# res += buffer[$t4]
+		addi 	$t0, 1			# Move to the next cell
+		addi 	$t4, 1			# Add 1 to index ($t4)
+		j parse_decimal_loop
+	parse_floating_loop:
+		j print_result
+
+
+
+	# Print resulting float
+	print_result:
+		li $v0, 4		# Display string
+		la $a0, result_float	# Display promot of what we are printing
+		syscall
+
+		li $v0, 2 		# Display a float
+		mov.s $f12, $f1		# store $f1 in f12
+		syscall
+
+		# Terminate the program
+		j exit
+
 
 
 exit:
